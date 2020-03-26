becd4f8c4ab50ce495b17a26c939d071f2c41c44
Bruno Santos <bcs.270@gmail.com>
2020-03-26 00:16:33 -0300

    projeto criado

---
+++ src/Program.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	File f = new File("C:\\in.txt");
+	Scanner c = null;
+	
+	
+
+	}
+
+}
